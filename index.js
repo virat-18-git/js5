131f701cc5439e0e9dc1f031bc2e0f0c0ada06c1
virat-18-git <ksprddy6@gmail.com>
2024-12-22 23:41:34 +0530

    operators2

---
+++ index.js
@@ -0,0 +1,124 @@
+// logical operators
+
+// let x1=50;
+// x2=100;
+// let result=!((x2>x1)&&(x1==x2))
+// let result=(x1>x2)||(x1==x2)
+// console.log(result)
+
+// let arr1=[1,2,5,"hloo",18,"virat"]
+// let arr2=[22,"shiva",99,"prasad",17,"raina",789,1000]
+// console.log((arr1.length<arr2.length)&&(arr1.length==arr2.length))
+
+
+// let obj1={
+//     name:"s1",
+//     salry:50000,
+// }
+// let obj2={
+//     name:"s2",
+//     slry:60000,
+// }
+// result=((obj1.salry<obj2.slry)&&(obj1==obj2))
+// result=((obj1.salry<obj2.slry)||(obj1==obj2))
+// result=!(((obj1.salry<obj2.slry)&&(obj1==obj2)))
+// console.log(result)
+
+
+// let java=true;
+// let python=true;
+// exp=5
+// relocate=true;
+// joinin=20;
+// let flexible=true;
+// let job=((java||python)&&(exp<=5)&&(relocate)||(joinin<=20)&&(flexible))
+// console.log(job)
+
+
+// conditional operators
+// 1.terinay operator
+// syntax:condition?true statement:flase statement;
+
+// let java=true;
+// let python=true;
+// exp=5
+// relocate=true;
+// joinin=20;
+// let flexible=true;
+// let job=((java||python)&&(exp<=5)&&(relocate)||(joinin<=20)&&(flexible))
+// console.log(job);
+// job?console.log("eligible for the job"):console.log("not eligible for the job")
+
+
+// let meeting=true;
+// meeting?console.log("completed"):console.log("incompleted")
+
+// let username="shiva";
+// password="shiva@222"
+// username=="shiva"&&password=="shiva@222"?console.log("login successfull"):console.log("login unsuccessfull")
+
+
+
+// 2.optional chaining:returns undefined
+// let obj={
+//     name:"virat",
+//     address:{
+//         h_no:1-43,
+//         street:"shivaji",
+//         city:"rajapet"
+
+//     },
+    // address1:null
+// }
+// console.log(obj.address1?.pincode)
+
+
+// let arr=[1,2,3,4,[5,6,7,8,9]]
+// console.log(arr[4][3])
+// console.log(arr[5]?.[2])
+
+
+
+// bitwise operators
+// let k=14&20;
+// console.log(k)
+
+// let p=10|12;
+// console.log(p)
+
+// let r=10^12;
+// console.log(r)
+
+
+
+
+login=true;
+user="admin";
+login?user=="customer"?
+console.log("welcome to customer"):
+console.log("welcome to admin"):
+console.log("please login");
+
+
+// let login=true;
+// let customer="veg"
+// let item="gobi"
+// login?
+// customer=="veg"?
+// item=="gobi"?
+// console.log("gobi"):
+// console.log("panner")
+
+// :item=="chicken"?
+// console.log("chicken"):
+// console.log("mutton")
+// :console.log("please login")
+
+
+
+
+
+
+
+
+
